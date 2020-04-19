35807eb9d289ab334c7dfb2af6fae325a7686668
Karan Patel <karan.patel1@rutgers.edu>
2020-04-18 21:57:16 -0400

    Initial Commit

---
+++ Weather Reporter/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Weather Reporter
+//
+//  Created by Karan Patel on 4/18/20.
+//  Copyright © 2020 Karan Patel. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Weather Reporter/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Weather Reporter
+//
+//  Created by Karan Patel on 4/18/20.
+//  Copyright © 2020 Karan Patel. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
